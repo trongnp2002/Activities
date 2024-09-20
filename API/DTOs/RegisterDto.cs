6850bd7df61e21e5b6bddedfad59beef81384068
Nguyễn Trọng <trongnp144@gmail.com>
2024-09-20 20:57:39 +0700

    End of section 12

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&/*$]).{8,20}$",ErrorMessage = "Password must be complex")]
+    public string Password { get; set; }
+    [Required]
+    public string DisplayName { get; set; }
+    [Required]
+    public string Username { get; set; }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,24 @@
 using API.Extensions;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
 
 // Add application services to the container
 builder.Services.AddApplicationService(builder.Configuration);
-
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +29,8 @@ if (app.Environment.IsDevelopment())
 }
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors("Policy");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -30,8 +40,9 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+    await Seed.SeedData(context, userManager);
     await Seed.SeedDataUserBookings(context);
 }
 catch (Exception ex)
