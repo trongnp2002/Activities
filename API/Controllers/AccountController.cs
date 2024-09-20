b20f9821bd654ea774f819637126f04095499c7a
Nguyễn Trọng <trongnp144@gmail.com>
2024-09-21 02:17:41 +0700

    Update response validation error account controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,11 +40,13 @@ namespace API.DTOs
         {
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
             {
-                return BadRequest("Username is already taken");
+                ModelState.AddModelError("Username", "Email taken");
+                return ValidationProblem();
             }
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email is already taken");
+                ModelState.AddModelError("Email", "Email taken");
+                return ValidationProblem();
             }
             var user = new AppUser
             {
