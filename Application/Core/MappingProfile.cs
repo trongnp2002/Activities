c3dc7ac597f196ab5f943974ecaa71b147fbb760
Nguyễn Trọng <trongnp144@gmail.com>
2024-09-24 16:54:24 +0700

    End of section 16.5

--- Application/Core/MappingProfile.cs
+++ Application/Core/MappingProfile.cs
@@ -8,14 +8,18 @@ namespace Application.Core
     {
         public MappingProfile()
         {
-            CreateMap<Activity, Activity>();
             CreateMap<Activity, ActivityDto>()
-            .ForMember(d => d.HostUserName, o => o.MapFrom(s => s.Attendees
-            .FirstOrDefault(x => x.IsHost).AppUser.UserName));
-            CreateMap<ActivityAttendee, Profiles.Profile>()
-           .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
-           .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
-           .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
+                .ForMember(d => d.HostUserName, o => o.MapFrom(s => s.Attendees
+                .FirstOrDefault(x => x.IsHost).AppUser.UserName));
+
+            CreateMap<ActivityAttendee, AttendeeDto>()
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+
+            CreateMap<AppUser, Profiles.Profile>()
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
 
 
         }
