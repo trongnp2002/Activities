dee2346a43f668ce7ec41b49157cf11eeb29191f
Nguyễn Trọng <trongnp144@gmail.com>
2024-09-24 12:39:52 +0700

    update cancel

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -14,6 +14,7 @@ public class ActivityDto
     public string City { get; set; }
     public string Venue { get; set; }
     public string HostUserName { get; set; }
+    public bool IsCancelled { get; set; }
     public ICollection<Profile> Attendees { get; set; }
 
 }
